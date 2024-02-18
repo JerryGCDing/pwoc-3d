febb82fcc43650e5c89879f80039d59b0a89f3cc
Jerry Ding <13386637338@qq.com>
2024-02-17 21:40:25 -0500

    eval cost fix

--- cost.py
+++ cost.py
@@ -3,14 +3,11 @@ from tensorflow.python.keras import backend
 from keras_flops import get_flops
 
 import network
-import datasets
 
 with backend.get_graph().as_default():
     net = network.Network()
 
-data = datasets.get_kitti_dataset(datasets.KITTI_VALIDATION_IDXS, batch_size=1)
-images, gt = next(data)
-inp = Input(images)
+inp = Input((375, 1242, 3))
 out = net(inputs=inp)
 model = Model(inp, out)
 
